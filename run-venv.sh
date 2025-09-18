cb67c274b598fb4a94248646d614420170381b28
Tubbles <jae91m@gmail.com>
2025-09-18 15:00:09 +0200

    Add venv

---
+++ run-venv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+set -euo pipefail
+
+my_dir="$(dirname "$(realpath "$0")")"
+venv_dir="${my_dir}/venv"
+
+if [[ ! -d "${venv_dir}" ]]; then
+    python3 -m venv venv
+    (source "${venv_dir}/bin/activate" && pip install -r "${my_dir}/requirements.txt")
+fi
+
+(source "${venv_dir}/bin/activate" && "${my_dir}/run.py" "$@")
